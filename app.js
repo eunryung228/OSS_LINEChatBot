18a6613aca4b31a893b494771cb63c266ca3b760
은승우 <sweun1@naver.com>
2019-12-01 22:48:34 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 const line = require('@line/bot-sdk');
-var greenlock=require('greenlock=express');
+var greenlock=require('greenlock-express');
 var server = greenlock.create({ version: 'v02', configDir: '/etc/letsencrypt', server: 'https://acme-v02.api.letsencrypt.org/directory', email: 'sweun1@naver.com', 
 approveDomains: ['www.oss.chatbot.bu.to', 'oss.chatbot.bu.to'], //my url, domain 
 agreeTos: true, renewWithin: 90 * 24 * 60 * 60 * 1000, renewBy: 89 * 24 * 60 * 60 * 1000, app: app }).listen(80, 443);
